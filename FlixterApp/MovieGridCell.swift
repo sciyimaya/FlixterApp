b4717fa6863a17fadd7673ac06f2b7aa361e838d
sciyimaya <ceydaiyimaya@gmail.com>
2021-02-19 10:44:37 -0500

    Flix Part 2 Completed

---
+++ FlixterApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixterApp
+//
+//  Created by Ceyda Iyimaya on 2/19/21.
+//  Copyright © 2021 Ceyda Iyimaya. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
